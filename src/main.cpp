cab0a5ee1c88b5d95917ea9f42f01374769b5c8e
Jonathan Knittel <jonknitt1205@gmail.com>
2024-12-02 18:41:04 -0500

    added printing of edges to example

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,23 @@ void createCase() {
     creator.createTestCase(10, 2, 123456789, "Output/test.txt");
 }
 
+void printAllEdges(const std::vector<std::vector<GraphEdge>>& adjList) {
+    for(size_t i = 0; i < adjList.size(); i++) {
+        for(const GraphEdge& edge : adjList.at(i)) {
+            std::cout << i << " - " << edge.cost << " -> " << edge.dest << std::endl;
+        }
+    }
+}
+
 int main() {
     createCase();
     CaseParser parser;
     parser.parseCase("Output/test.txt");
 
     const std::vector<std::vector<GraphEdge>>& adjList = parser.getAdjList();
-
     
+    // print all edges just to prove it all worked lol
+    printAllEdges(adjList);
 
     // here you would call the algos
 }
